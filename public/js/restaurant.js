53a46a587c24a5bca2a81c18379643b99f3702b3
Vinicius <vinidroid@gmail.com>
2016-01-05 23:18:51 +1100

    First get yeee

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -1,1 +1,49 @@
-angular.module('restaurant', ['ui.router'])
+function routerConfig($urlRouterProvider, $stateProvider){
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('list', {
+      url:'/',
+      templateUrl: 'partial-list.html',
+      controller: 'MainCtrl'
+    })
+}
+
+function MainCtrl($scope, RestaurantService){
+
+  var self           = this;
+  $scope.getList     = getList;
+  $scope.activator   = activator;
+
+  activator();
+
+  function activator(){
+      getList();
+  }
+
+
+  function getList(){
+    RestaurantService.getRestaurants().then(function(result){
+        $scope.restaurants = result.data;
+    });
+  }
+}
+
+function RestaurantService( $http, $resource ) {
+
+
+    var Service = {};
+
+    Service.getRestaurants = function(){
+
+        return $http.get('http://localhost:3000/restaurants');
+    };
+
+    return Service;
+}
+
+
+angular.module('restaurant', ['ui.router', 'ngResource'])
+  .controller('MainCtrl', MainCtrl)
+  .factory('RestaurantService', RestaurantService)
+  .config(routerConfig)
